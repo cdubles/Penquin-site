f6b2eb669c058dc871dd2cb88c683032fb74503c
cdubles <33189090+cdubles@users.noreply.github.com>
2019-11-02 23:58:43 -0400

    simple update

--- JS.js
+++ JS.js
@@ -1,3 +1,4 @@
+//runs on doc ready
 $(function(){
   $("button#dropbtn").click(function(){
     $("div.dropdown-content").css("display","block");
@@ -15,7 +16,7 @@ $(function(){
     for (i = 0; i < tabcontent.length; i++) {
       tabcontent[i].style.display = "none";
     }
-
+    
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
       tablinks[i].className = tablinks[i].className.replace("active", "");
