d2b17fe2e78cbba3905892d0adc036cd2cc1c5d3
cdubles <33189090+cdubles@users.noreply.github.com>
2019-11-02 20:22:25 -0400

    init

---
+++ JS.js
@@ -0,0 +1,22 @@
+$(function(){
+
+});
+
+  //drop down controls
+  function tabs(evt, tab) {
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    console.log(tabcontent.length);
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace("active", "");
+    }
+
+    document.getElementById(tab).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
